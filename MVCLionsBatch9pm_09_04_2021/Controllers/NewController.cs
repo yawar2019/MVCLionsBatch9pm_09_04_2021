d2b1bcb6c456d43535b2dc295444166893a6e28d
yawar2019 <yawarali17@gmail.com>
2021-09-08 22:06:08 +0530

    committed

--- MVCLionsBatch9pm_09_04_2021/Controllers/NewController.cs
+++ MVCLionsBatch9pm_09_04_2021/Controllers/NewController.cs
@@ -9,7 +9,7 @@ namespace MVCLionsBatch9pm_09_04_2021.Controllers
 {
     public class NewController : Controller
     {
-        // GET: kanupriya got latest Code
+        // GET: shym got latest Code
         public ActionResult Index()
         {
             EmployeeModel emp = new EmployeeModel();
